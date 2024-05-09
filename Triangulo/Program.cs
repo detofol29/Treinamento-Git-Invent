85f762247db2db34233698a33c5d2c121c50e355
Gabriel Detofol <detofol29@gmail.com>
2024-05-09 11:47:59 -0300

    txt

---
+++ Triangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+class URI
+{
+    static void Main(string[] args)
+    {
+        double A, B, C;
+        string inp = Console.ReadLine();
+        double[] valores = inp.Split(' ').Select(x => double.Parse(x)).ToArray();
+        A = valores[0];
+        B = valores[1];
+        C = valores[2];
+
+        if ((Math.Abs(B - C) < A && A < (B + C)) && (Math.Abs(A - C) < B && B < (A + C)) && (Math.Abs(A - B) < C && C < (A + B)))
+        {
+            double perimetro = A + B + C;
+            Console.WriteLine("Perimetro = {0:F1}", perimetro);
+
+        }
+        else
+        {
+            double area = (A + B) * C / 2;
+            Console.WriteLine("Area = {0:F1}", area);
+        }
+
+    }
+}
